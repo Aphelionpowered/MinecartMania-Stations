2f42aefc40b7f184c09bff3da9517e954680e7d5
N3X15 <nexis@7chan.org>
2011-07-21 15:05:06 -0700

    Try to fix weird method not found error

--- src/com/afforess/minecartmaniastation/StationCondition.java
+++ src/com/afforess/minecartmaniastation/StationCondition.java
@@ -10,74 +10,87 @@ import com.afforess.minecartmaniacore.utils.ItemUtils;
 
 public enum StationCondition implements Condition {
     Default {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return str.equals("D") || str.toLowerCase().contains("default");
         }
     },
     Empty {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.isStandardMinecart() && minecart.minecart.getPassenger() == null && str.toLowerCase().contains("empty");
         }
         
     },
     Player {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.hasPlayerPassenger() && str.toLowerCase().contains("player");
         }
         
     },
     Mob {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.minecart.getPassenger() != null && !minecart.hasPlayerPassenger() && str.toLowerCase().contains("mob");
         }
     },
     Pig {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.minecart.getPassenger() instanceof org.bukkit.entity.Pig && str.toLowerCase().contains("pig");
         }
     },
     Chicken {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.minecart.getPassenger() instanceof org.bukkit.entity.Chicken && str.toLowerCase().contains("chicken");
         }
     },
     Cow {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.minecart.getPassenger() instanceof org.bukkit.entity.Cow && str.toLowerCase().contains("cow");
         }
     },
     Sheep {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.minecart.getPassenger() instanceof org.bukkit.entity.Sheep && str.toLowerCase().contains("sheep");
         }
     },
     Creeper {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.minecart.getPassenger() instanceof org.bukkit.entity.Creeper && str.toLowerCase().contains("creeper");
         }
     },
     Skeleton {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.minecart.getPassenger() instanceof org.bukkit.entity.Skeleton && str.toLowerCase().contains("skeleton");
         }
     },
     Zombie {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.minecart.getPassenger() instanceof org.bukkit.entity.Zombie && str.toLowerCase().contains("zombie");
         }
     },
     StationCommand {
-        
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.hasPlayerPassenger() && SignCommands.processStationCommand(minecart, str);
         }
     },
     PlayerName {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.hasPlayerPassenger() && str.equalsIgnoreCase(minecart.getPlayerPassenger().getName());
         }
     },
-    ContainsItem {      
+    ContainsItem {     
+        @Override 
         public boolean result(MinecartManiaMinecart minecart, String str) {
             if (minecart.hasPlayerPassenger() && minecart.getPlayerPassenger().getItemInHand() != null) {
                 Item itemInHand = Item.getItem(minecart.getPlayerPassenger().getItemInHand().getTypeId(), minecart.getPlayerPassenger().getItemInHand().getDurability());
@@ -99,44 +112,51 @@ public enum StationCondition implements Condition {
         }
     },
     Cargo {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.isStorageMinecart() && str.toLowerCase().contains("cargo") && ((MinecartManiaStorageCart) minecart).isEmpty();
         }
     },
     Storage {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.isStorageMinecart() && str.toLowerCase().contains("storage");
         }
     },
     Powered {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return minecart.isPoweredMinecart() && str.toLowerCase().contains("powered");
         }
     },
     West {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return (str.equals("W") || str.toLowerCase().contains("west")) && !str.contains("-") && minecart.getDirection() == CompassDirection.WEST;
         }
     },
     East {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return (str.equals("E") || str.toLowerCase().contains("east")) && !str.contains("-") && minecart.getDirection() == CompassDirection.EAST;
         }
     },
     North {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return (str.equals("N") || str.toLowerCase().contains("north")) && !str.contains("-") && minecart.getDirection() == CompassDirection.NORTH;
         }
     },
     South {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return (str.equals("S") || str.toLowerCase().contains("south")) && !str.contains("-") && minecart.getDirection() == CompassDirection.SOUTH;
         }
     },
     Redstone {
+        @Override
         public boolean result(MinecartManiaMinecart minecart, String str) {
             return str.toLowerCase().contains("redstone") && (minecart.isPoweredBeneath() || MinecartManiaWorld.isBlockIndirectlyPowered(minecart.minecart.getWorld(), minecart.getX(), minecart.getY() - 2, minecart.getZ()));
         }
     }
-    
 }
